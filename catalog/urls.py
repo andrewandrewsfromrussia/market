91bd66e468662543007be6ff80249198ec470c10
andrew.andrews <andrewardyour@gmail.com>
2025-09-28 10:31:59 +0500

    feat: product detail page (controller, url, template)

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from . import views
 from catalog.apps import CatalogConfig
 from catalog.views import home, contacts
 
@@ -6,5 +7,6 @@ app_name = CatalogConfig.name
 
 urlpatterns = [
     path("", home, name="home"),
-    path("contacts/", contacts, name="contacts")
+    path("contacts/", contacts, name="contacts"),
+    path("products/<int:pk>/", views.product_detail, name="product_detail"),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,7 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Product
 
 def home(request):
     return render(request, "home.html")
 
 def contacts(request):
     return render(request, "contacts.html")
+
+def product_detail(request, pk: int):
+    product = get_object_or_404(Product, pk=pk)
+    return render(request, "catalog/product_detail.html", {"product": product})
